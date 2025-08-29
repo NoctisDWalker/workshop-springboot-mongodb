6932ef4d58772bb637524b45c57d28e391635b0a
Filipe dos Santos <filipeuvft@gmail.com>
2025-08-29 19:43:02 -0300

    Created project

---
+++ src/test/java/com/nerdev/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdev.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
