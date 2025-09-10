4ca9d97623b8856fdeef0762c2f3161a1fb5fed4
Filipe dos Santos <filipeuvft@gmail.com>
2025-09-10 20:13:52 -0300

    Simple query methods

--- src/main/java/com/nerdev/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nerdev/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.nerdev.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.nerdev.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
