9fe763ccbb434ba206b88f533119481059e47335
Filipe dos Santos <filipeuvft@gmail.com>
2025-09-09 21:23:07 -0300

    Return post by id

---
+++ src/main/java/com/nerdev/workshopmongo/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.nerdev.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nerdev.workshopmongo.domain.Post;
+import com.nerdev.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService postservice;
+
+	@GetMapping(value="/{id}")
+ 	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = postservice.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/nerdev/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.nerdev.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nerdev.workshopmongo.domain.Post;
+import com.nerdev.workshopmongo.repository.PostRepository;
+import com.nerdev.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+}
